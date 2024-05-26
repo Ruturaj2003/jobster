4d416f6080382d3098a2beb84c3ae65802ef05e5
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-26 18:57:35 +0530

    Create And Setup From Row Component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Logo } from '../components';
+import { FormRow, Logo } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
@@ -30,54 +30,33 @@ const Register = () => {
     e.preventDefault();
     console.log(values);
   };
+
   return (
     <Wrapper className="full-page">
       <form onSubmit={onSubmit} className="form">
         <Logo></Logo>
         <h3>Login</h3>
 
-        {/* Name Field */}
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            value={values.name}
-            className="form-input"
-            onChange={(e) => handleChange(e, 'name')}
-            name="name"
-          ></input>
-        </div>
-        {/* Email Field */}
-        <div className="form-row">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            value={values.email}
-            className="form-input"
-            onChange={(e) => handleChange(e, 'email')}
-            name="email"
-          ></input>
-        </div>
+        <FormRow
+          name={'name'}
+          type={'text'}
+          handleChange={handleChange}
+          value={values.name}
+        ></FormRow>
 
-        {/* Password Field */}
-        <div className="form-row">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            minLength={'5'}
-            maxLength={'16'}
-            value={values.password}
-            className="form-input"
-            onChange={(e) => handleChange(e, 'password')}
-            name="password"
-          ></input>
-        </div>
+        <FormRow
+          type={'email'}
+          name={'email'}
+          handleChange={handleChange}
+          value={values.email}
+        ></FormRow>
+
+        <FormRow
+          type={'password'}
+          name={'password'}
+          handleChange={handleChange}
+          value={values.password}
+        ></FormRow>
 
         <button type="submit" className="btn btn-block">
           Submit
