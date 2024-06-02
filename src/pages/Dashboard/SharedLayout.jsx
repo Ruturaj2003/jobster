c8dcdafd4ccdae49edc9834a00c66c4fdbe1bb4a
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-06-02 11:40:02 +0530

    Setup Navbar, Small Sidebar, BigSidebar in Shared Layout

--- src/pages/Dashboard/SharedLayout.jsx
+++ src/pages/Dashboard/SharedLayout.jsx
@@ -1,4 +1,23 @@
+import { Outlet } from 'react-router-dom';
+import { BigSidebar, Navbar, SmallSidebar } from '../../components';
+import Wrapper from '../../assets/wrappers/SharedLayout';
+
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <>
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div className="">
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
+  );
 };
 export default SharedLayout;
